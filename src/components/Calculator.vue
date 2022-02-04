70cf3e3011e1b40069aa1b168bc3f18c8da5212a
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2022-02-04 15:31:49 +0100

    create a equal method and end of the project

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -48,6 +48,11 @@ export default {
     },
 
     append(number) {
+      if(this.operatorClicked) {
+        this.current = '';
+        this.operatorClicked = false;
+      }
+
       this.current = `${this.current}${number}`;
     },
 
@@ -64,22 +69,31 @@ export default {
 
     divide() {
       this.operator = (a, b) => a / b;
+      this.setPrevious();
     },
 
     times() {
       this.operator = (a, b) => a * b;
+      this.setPrevious();
     },
 
     minus() {
       this.operator = (a, b) => a - b;
+      this.setPrevious();
     },
 
     add() {
       this.operator = (a, b) => a + b;
+      this.setPrevious();
     },
 
     equal() {
+      this.current = `${this.operator(
+        parseFloat(this.current),
+        parseFloat(this.previous)
+      )}`;
 
+      this.previous = null;
     }
   }
 }
