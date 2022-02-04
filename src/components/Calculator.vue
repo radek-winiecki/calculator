a6db26ee42c4687daa4aaa297310a228db7907c5
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2022-02-04 15:06:55 +0100

    add a display of numbers after clicking

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -5,19 +5,19 @@
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
     <div class="btn operator">÷</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
-    <div class="btn operator">x</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
-    <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div @click="append()" class="btn operator">x</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div @click="append()" class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
     <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
+    <div @click="append('0')" class="btn zero">0</div>
     <div class="btn">.</div>
     <div class="btn operator">=</div>
   </div>
@@ -42,6 +42,10 @@ export default {
 
     percent() {
       this.current = `${parseFloat(this.current) / 100}`;
+    },
+
+    append(number) {
+      this.current = `${this.current}${number}`;
     }
   }
 }
